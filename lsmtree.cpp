0df1981367e039265d7f964ba649aa9cb48af7f5
rafathasan <33374525+rafathasan@users.noreply.github.com>
2020-04-02 21:31:59 +0600

    Create lsmtree.cpp

---
+++ lsmtree.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : lsmtree.cpp
+// Author      : Rafat Hasan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "lsmdef.h"
+
+using namespace std;
+using namespace lsm;
+
+
+int main() {
+	FileEngine fe;
+	fe.getmeta();
+	return 0;
+}
+
+
+
